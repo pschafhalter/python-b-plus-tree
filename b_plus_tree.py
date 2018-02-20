5a132b65f4ca0810aa90fd5aa5f663e1d7424784
Peter Schafhalter <pschafhalter@berkeley.edu>
2018-02-19 16:11:07 -0800

    Implement splitting tree

--- b_plus_tree.py
+++ b_plus_tree.py
@@ -1,3 +1,5 @@
+import bisect
+
 from b_plus_node import Leaf, Node
 
 
@@ -6,7 +8,6 @@ class BPlusTree:
         self.branching_factor = branching_factor
         self.leaves = Leaf(None, None, None, branching_factor) # Linked List of leaves
         self.root = self.leaves
-        self.num_items = 0
 
     def get(self, key):
         return self.root.get(key)
@@ -18,7 +19,6 @@ class BPlusTree:
         self.root.remove_item(key)
         if type(self.root) is Node and len(self.root.children) == 1:
             self.root = self.root.children[0]
-        self.num_items -= 1
 
     def __delitem__(self, key):
         return self.remove_item(key)
@@ -27,7 +27,6 @@ class BPlusTree:
         self.root.set(key, value)
         if self.root.parent is not None:
             self.root = self.root.parent
-        self.num_items += 1
 
     def __setitem__(self, key, value):
         return self.set(key, value)
@@ -38,3 +37,39 @@ class BPlusTree:
         while leaf is not None:
             result += leaf.size()
             leaf = leaf.next
+        return result
+
+    def split(self, key):
+        tree = BPlusTree()
+        tree.root = Node(None, None, [], [], self.branching_factor)
+
+        current_node = self.root
+        new_node = tree.root
+        while type(current_node) is Node or type(current_node) is Leaf:
+            child_type = type(current_node.children[0])
+            split_index = bisect.bisect_left(current_node.keys, key)
+            new_node.keys = current_node.keys[:split_index]
+            new_node.children = current_node.children[:split_index]
+            current_node.keys = current_node.keys[split_index:]
+            current_node.children = current_node.children[split_index:]
+            if len(current_node.children) == 0:
+                break
+            # Add new ambiguous node on the split and fix pointers
+            if child_type is Node:
+                new_node.children.append(Node(new_node.children[-1], None, [], [],
+                                              parent=new_node,
+                                              branching_factor=new_node.branching_factor))
+                new_node.children[-2].next = new_node.children[-1]
+                current_node.children[0].previous = None
+            elif child_type is Leaf:
+                new_node.children.append(Leaf(new_node.children[-1], None, new_node,
+                                              branching_factor=new_node.branching_factor))
+                new_node.children[-2].next = new_node.children[-1]
+                current_node.children[0].previous = None
+            # Balance trees
+            new_node.balance()
+            current_node.balance()
+            current_node = current_node.children[0]
+            new_node = new_node.children[-1]
+
+        return tree
